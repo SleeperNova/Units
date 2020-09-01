35fc8ae24a43c1b62c13a3d285acbf5830699b1a
Francisco Saavedra <saavedra.francisco@outlook.com>
2020-09-01 15:15:05 -0500

    organized code and renamed variables

--- 03-JavaScript/Develop_HW/script.js
+++ 03-JavaScript/Develop_HW/script.js
@@ -1,79 +1,97 @@
-//Assignment code
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-//Declare password global variable
-var password = (' ');
-
-//Call to writePassword on page which invokes generatePassword function
-function writePassword() {
-  generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
-};
-
-//Main javascript to create password possibly composed of upper, lower, numeric, and special characters
-function generatePassword() {
-  //Create arrays for each possible character type
-    const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const lower = "abcdefghijklmnopqrstuvwxyz";
-    const numbers = "0123456789";
-    const special = "#$%;&'()*+,-./:;<=>?@[\]^_`{|}~";
-
-    //Keep prompting user if length is not between 8 and 128
-    do {
-      var length = prompt("Enter length of password between 8 and 128");
-    }  while ((length < 8) || (length > 128));
-
-    //Keep prompting user if at least one character type is not chosen
-    do {
-      var cnfrmupper = confirm("Would you like upper case in password?");
-      var cnfrmlower = confirm("Would you like lower case in password?");
-      var cnfrmnumbers = confirm("Would you like numbers in password?");
-      var cnfrmspecial = confirm("Would you like special characters in password?");
-      var pwdChars = [];
-    } while ((cnfrmupper === false) && (cnfrmlower === false) && (cnfrmnumbers === false) && (cnfrmspecial === false));
-
-    //Append array given user choices from confirms above
-    if(cnfrmupper) {
-      pwdChars = [...pwdChars,...upper];
-    };
-    if(cnfrmlower) {
-      pwdChars = [...pwdChars,...lower];
-    };
-    if(cnfrmnumbers) {
-      pwdChars = [...pwdChars,...numbers];
-    };  
-    if(cnfrmspecial) {
-      pwdChars = [...pwdChars,...special];
-    };
-    // console.log(pwdChars);
-    
-    //Build password given length chosen and random pick of character type array
-    for (i = 0; i < length; i++) {		
-      var randomNumber = Math.floor(Math.random() * pwdChars.length);
-      console.log(randomNumber);
-      var randomPick = pwdChars[randomNumber];
-      // console.log(randomPick);
-      password = password + randomPick;
-      // console.log(randomPick);
-    }; 
-  };
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-//generateBtn.addEventListener("click", erasePassword);
 
-//copy to clipboard and clear
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+function generatePassword() {
+  var confirmLength = prompt(
+    "How many characters would you like your password to be?",
+    "Choose a number between 8 and 128"
+  );
+  confirmLength = parseInt(confirmLength);
+  // confrim lenth is more than 8 OR less than 128
+  if (confirmLength < 8 || confirmLength > 128) {
+    alert("Please choose a number between 8 and 128.");
+    return generatePassword();
+  }
+
+  var Upper = confirm("Would you like to include upper case letters?");
+  var Lower = confirm("Would you like to include lower case letters?");
+  var Number = confirm("Would you like to include numbers?");
+  var Symbol = confirm("Would you like to include symbols?");
+
+  // checking values to make sure at least one value was selected
+  if (!(Upper || Lower || Number || Symbol)) {
+    alert("Please select a minimum of one option");
+    return generatePassword();
+  }
+  // defining passwords as a blank value that can be filled
+  var password = "";
+  // concat
+  while (password.length < confirmLength) {
+    if (Upper) {
+      // concat together (non-array concat)
+      password += getRandomUpper();
+      // once it reaches the number entered it breaks the cycle
+      if (password.length === confirmLength) break;
+    }
+    if (Lower) {
+      password += getRandomLower();
+      if (password.length === confirmLength) break;
+    }
+    if (Number) {
+      password += getRandomNumber();
+      if (password.length === confirmLength) break;
+    }
+    if (Symbol) {
+      password += getRandomSymbol();
+      if (password.length === confirmLength) break;
+    }
+  }
+
+  // turns string into an array to shuffle (ensuring one of each but mixing them up)
+  password = password.split("");
+  // for loop i = length not 0 to avoid issues
+  for (var i = password.length; i < password.length; i++) {
+    var randIndex = Math.floor(Math.random() * password.length);
+    var temp1 = password[i];
+    var temp2 = password[randIndex];
+    // shuffle
+    password[i] = temp2;
+    password[randIndex] = temp1;
+  }
+  // turns the array back into a string
+  return password.join("");
+}
+
+// Random Generation
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+function getRandomSymbol() {
+  const symbols = "!@#$%^&*()";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+//copy to clipboard w/ optional line to clear
 function copyPassword() {
   var copyText = document.getElementById("password");
   copyText.select();
   document.execCommand("Copy");
   alert("Copied to Clipboard");
-  document.getElementById("password").value = "";
-  //document.getElementById("writePassword").value = "";
+  //document.getElementById("password").value = "";
 }
-
-/*
-function erasePassword() {
-  document.getElementById('password').value = "";
-} */
